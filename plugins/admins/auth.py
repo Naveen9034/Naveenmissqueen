34a4d302c95f7ba8589584b412eda9763d5567b7
Naveen9034 <153264778+Naveen9034@users.noreply.github.com>
2023-12-09 03:18:04 +0530

    Update auth.py

--- plugins/admins/auth.py
+++ plugins/admins/auth.py
@@ -1,7 +1,7 @@
-# Powered By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Updates
-# Join @AdityaCheats For Hacks
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
+# Powered By @ashif903 & @mr_naveen720 
+# Join @ODDRAGONS For More Updates
+# Join @Ashif903 For Hacks
+# Join Our Chats @ODDRAGONS & @mr_naveen720 
 
 
 from pyrogram import filters
@@ -155,7 +155,7 @@ async def authusers(client, message: Message, _):
         await message.reply_text(text)
 
 
-# Powered By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Updates
-# Join @AdityaCheats For Hacks
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
+# Powered By @ashif903 & @mr_naveen720 
+# Join @ODDRAGONS For More Updates
+# Join @Ashif903 For Hacks
+# Join Our Chats @ODDRAGONS & @mr_naveen720
