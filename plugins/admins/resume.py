3b0821d5821420b9909ad34f6aa9e9e957557e00
Naveen9034 <153264778+Naveen9034@users.noreply.github.com>
2023-12-09 03:23:15 +0530

    Update resume.py

--- plugins/admins/resume.py
+++ plugins/admins/resume.py
@@ -1,7 +1,7 @@
-# Powered By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Updates
-# Join @AdityaCheats For Hacks
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
+# Powered By @ashif903 & @mr_naveen720 
+# Join @ODDRAGONS For More Updates
+# Join @Ashif903 For Hacks
+# Join Our Chats @ODDRAGONS & @mr_naveen720 
 
 from pyrogram import filters
 from pyrogram.types import Message
@@ -37,7 +37,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     )
 
 
-# Powered By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Updates
-# Join @AdityaCheats For Hacks
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
+# Powered By @ashif903 & @mr_naveen720 
+# Join @ODDRAGONS For More Updates
+# Join @Ashif903 For Hacks
+# Join Our Chats @ODDRAGONS & @mr_naveen720 
