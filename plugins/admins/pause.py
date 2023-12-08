7fd06ec05d1ab6cc7cae2e44b1de0a45c8a56655
Naveen9034 <153264778+Naveen9034@users.noreply.github.com>
2023-12-09 03:22:13 +0530

    Update pause.py

--- plugins/admins/pause.py
+++ plugins/admins/pause.py
@@ -1,7 +1,7 @@
-# Powered By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Updates
-# Join @AdityaCheats For Hacks
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
+# Powered By @ashif903 & @mr_naveen720 
+# Join @ODDRAGONS For More Updates
+# Join @Ashif903 For Hacks
+# Join Our Chats @ODDRAGONS & @mr_naveen720 
 
 
 from pyrogram import filters
@@ -38,7 +38,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     )
 
 
-# Powered By @BikashHalder & @AdityaHalder 
-# Join @BikashGadgetsTech For More Updates
-# Join @AdityaCheats For Hacks
-# Join Our Chats @Bgt_Chat & @Adityadiscus 
+# Powered By @ashif903 & @mr_naveen720 
+# Join @ODDRAGONS For More Updates
+# Join @Ashif903 For Hacks
+# Join Our Chats @ODDRAGONS & @mr_naveen720 
